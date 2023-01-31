a72e79b609f6a50f866186f40671bb4f2ea0e043
desafiopraticotrybe <123988540+desafiopraticotrybe@users.noreply.github.com>
2023-01-31 11:06:32 -0300

    Update members.stub.js

--- tests/integration/doubles/members.stub.js
+++ tests/integration/doubles/members.stub.js
@@ -1,1 +1,34 @@
 // src/tests/integration/doubles/members.stub.js
+
+const membersList = [
+  {
+    id: 1,
+    first_name: 'Carlos Márcio',
+    last_name: 'Russo',
+    email: 'cmrusso@email.com',
+    phone: '51992824816',
+  },
+  {
+    id: 2,
+    first_name: 'Adão',
+    last_name: 'Ferreira',
+    email: 'adaofer@email.com',
+    phone: '21985336211',
+  },
+  {
+    id: 3,
+    first_name: 'Jandira',
+    last_name: 'Soares',
+    email: 'jandiras@email.com',
+    phone: '48994325998',
+  },
+];
+
+const monthlyRevenue = {
+  monthlyRevenue: '89.70',
+};
+
+module.exports = { 
+  membersList,
+  monthlyRevenue,
+ };
