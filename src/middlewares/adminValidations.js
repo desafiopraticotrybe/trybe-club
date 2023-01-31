bdc490fff156c070378525a9a804717f1af0dc12
desafiopraticotrybe <123988540+desafiopraticotrybe@users.noreply.github.com>
2023-01-31 11:05:06 -0300

    Update adminValidations.js

--- src/middlewares/adminValidations.js
+++ src/middlewares/adminValidations.js
@@ -1,1 +1,20 @@
 // src/middlewares/adminValidations.js
+
+const validateLoginUser = (req, res, next) => {
+  const { user } = req.body;
+  if (!user) return res.status(400).json({ message: 'user não informado.' });
+  if (user !== 'admin') return res.status(401).json({ message: 'user inválido.' });
+  next();
+};
+
+const validateLoginPassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) return res.status(400).json({ message: 'password não informado.' });
+  if (password !== 'xablau') return res.status(401).json({ message: 'password inválido.' });
+  next();
+};
+
+module.exports = {
+  validateLoginUser,
+  validateLoginPassword,
+};
